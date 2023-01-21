94906e7f5ccd3ccde35d5fd6d06dc740bcd1b538
Fedejalife99 <fedejalife@gmail.com>
2023-01-21 14:08:29 -0800

    fix task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print(0,end ='')
-        print(i,end =', ')
-        continue
-    if i == 99:
-        print("{}".format(i), end='\n')
-        continue
-    print("{}".format(i), end= ', ')
+        print("0", end='')
+    print(i, end=', ' if i != 99 else '\n')
