049c37b0af0318b5a1592d7bd1b38f4ac3e2a511
Fedejalife99 <fedejalife@gmail.com>
2023-01-21 15:49:07 -0800

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
-            continue
+            print("FizzBuzz ", end = '')
         if i % 3 == 0:
             print("Fizz ", end = '')
             continue
