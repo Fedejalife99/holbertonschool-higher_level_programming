845415dca42edc98425c2add73cfcabe3bc63f22
Fedejalife99 <fedejalife@gmail.com>
2023-01-21 14:48:54 -0800

    task 6 fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,7 @@
 for i in range(10):
   for j in range(i + 1, 10):
     if i == 8 and j == 9:
-      print(i,end= '')
-      print(j)
+      print("{}{}".format(i, j),end= '\n')
       continue
     print("{}".format(i), end='')
     print("{}".format(j), end = ', ')
