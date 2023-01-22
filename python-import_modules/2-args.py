8f34372573f8ee7373661729bed05a7e4a73c55a
Fedejalife99 <fedejalife@gmail.com>
2023-01-22 10:18:23 -0800

    task 2 fix codestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,9 @@ if __name__ == "__main__":
     num_args = len(argv)
     if num_args == 0:
         print("0 arguments.")
-    
     if num_args != 0 and num_args != 1:
         print(f"{num_args} arguments:")
     if num_args == 1:
         print(f"{num_args} argument:")
-    for i in range (1, num_args + 1):
+    for i in range(1, num_args + 1):
         print("{}: {}".format(i, argv[i - 1]))
