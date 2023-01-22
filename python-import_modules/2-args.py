d57c1ffb58f3a674e5429ea59d5db22c9253b3d9
Fedejalife99 <fedejalife@gmail.com>
2023-01-22 10:05:37 -0800

    task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    if num_args == 0:
+        print("0 arguments.")
+    if num_args != 0:
+        print(f"{num_args} arguments")
+        for i in range (1, num_args + 1):
+            print("{}: {}".format(i, argv[i - 1]))
