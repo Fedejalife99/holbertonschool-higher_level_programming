32b31f219888f56f7bed309c0cb421b5566cdf40
Fedejalife99 <fedejalife@gmail.com>
2023-01-24 08:32:10 -0800

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) > 1:
+    if not my_list:
+        return None
+    else:
         max_int = my_list[0]
         for i in range(1, len(my_list)):
             if my_list[i] > max_int:
