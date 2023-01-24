aa8993d40a94db355ef16e5a7d3ca23c8bf35a2b
Fedejalife99 <fedejalife@gmail.com>
2023-01-24 07:54:35 -0800

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,6 +5,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) >= 2 and len(tuple_a) == 1:
         frst_elem += tuple_b[0]
         sec_elem += 0
+    if len(tuple_b) == 1 and len(tuple_a) == 1:
+        frst_elem += tuple_b[0] + tuple_a[0]
+        sec_elem += 0
     if len(tuple_a) >= 2 and len(tuple_b) == 1:
         frst_elem += tuple_a[0] + tuple_b[0]
         sec_elem += tuple_a[1]
