bc0e89fb7305a5d8b8e65879a2ae6673ccc6a233
Federico Jalife <fedejalife@gmail.com>
2023-01-27 11:49:04 -0300

    task 0 fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,6 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     index = 0
     try:
+        if x == 0:
+            return 0
         for i in range(x):
             print("{}".format(my_list[i]), end='')
         i += 1
