742299cb86764f3b8cbd439e29f46939608bccc8
Federico Jalife <fedejalife@gmail.com>
2023-01-27 13:13:58 -0300

    task 2 empty list fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    if x == 0:
+        print()
+        return 0
+    if my_list is not my_list:
+        print()
+        return 0
     for elements in range(x):
         try:
             print("{:d}".format(my_list[elements]), end='')
@@ -8,6 +14,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 return elements + 1
         except (TypeError, ValueError):
             elements += 1
+            continue
 
         
     
