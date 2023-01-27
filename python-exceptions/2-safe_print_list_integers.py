d69006239362670ad0c47d8f9cb73ddbec1f3f11
Federico Jalife <fedejalife@gmail.com>
2023-01-27 13:55:44 -0300

    pycodestyle task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for elements in range(x):
         try:
             print("{:d}".format(my_list[elements]), end='')
-            if elements + 1 == x :
+            if elements + 1 == x:
                 print()
                 return elements + 1
         except (TypeError, ValueError):
