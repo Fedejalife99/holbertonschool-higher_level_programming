6854feb5d94d0a153252b02750ff7cbdc2f5335a
Federico Jalife <fedejalife@gmail.com>
2023-01-30 11:24:22 -0300

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a class square"""
+class Square:
+    """Square class with private instance attribute: size"""
+
+
+    def __init__(self, size):
+        """args: size of the square"""
+        self.__size = size
