7b4d8d6b2a6758288dd5c8ae797b1bb8fee74a3e
Federico Jalife <fedejalife@gmail.com>
2023-01-30 11:13:03 -0300

    task 0 doc fix 3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """define a class square"""
 class Square:
+    """empty Square class"""
     def __init__(self) -> None:
-        """empty Square class"""
-        pass
+                pass
