2794702667eabd65aee335b823cbbb3a64bfaa99
Federico Jalife <fedejalife@gmail.com>
2023-01-31 14:34:50 -0300

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""define a class rectangle"""
+
+class Rectangle:
+    """Rectangle class with width and height as arguments"""
+
+    def __init__(self, width = 0, height = 0):
+        """inialitization of the rectangle
+            args:
+                height: value of the height of the rectangle
+                width: value of the width of the rectangle
+        """
+        self.height = height
+        self.width = width
+    
+    @property
+    def height(self):
+        """set value of height"""
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value == 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    @property
+    def width(self):
+        """set value of width"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value == 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+        
+    
+
