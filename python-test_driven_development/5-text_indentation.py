8f78ca79e64d12bb72dba14ca7d9d845d8174f99
Federico Jalife <fedejalife@gmail.com>
2023-02-03 12:37:20 -0300

    task 5 and  tests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,9 @@
+def text_indentation(text):
+    if not isinstance(text,str):
+        raise TypeError("text must be a string")
+    for letter in text:
+        if letter in [".", "?", ":"]:
+            print('\n')
+        else:
+            print(letter,end='')
+    
