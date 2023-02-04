8cde4ee62b51bddb3019bc88c80b021399935723
Federico Jalife <fedejalife@gmail.com>
2023-02-04 11:05:09 -0300

    tas 0 pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """define a class square"""
 
-
 class Square:
     """empty Square class"""
 
-
-    def __init__(self) -> None:
+def __init__(self) -> None:
         pass
+        
