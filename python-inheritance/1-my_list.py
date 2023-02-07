0fbd2fe02ad57f2f963e11d0ea9eb49d4a144152
Federico Jalife <fedejalife@gmail.com>
2023-02-07 12:07:02 -0300

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/ust/bin/python3
+"""define a child class from list"""
+
+
+class MyList(list):
+    """class from list class thats print the parent class in ascending order"""
+    def print_sorted(self):
+        print(sorted(self))
