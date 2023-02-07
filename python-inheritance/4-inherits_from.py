71c18809e40ebfd73e4cab5bd838cdb77e642178
Federico Jalife <fedejalife@gmail.com>
2023-02-07 12:58:38 -0300

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""funtion that verify if a object inherit from a parent class"""
+
+def inherits_from(obj, a_class):
+    """return true if inherit from a parent or false if not"""
+    if inherits_from(obj, a_class):
+        return True
+    return False
