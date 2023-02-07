e5aaa9dfb3cdc557f5dc243cb457079c0fc10ca3
Federico Jalife <fedejalife@gmail.com>
2023-02-07 12:22:32 -0300

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a function that compares the methods and attributes of two objects"""
+
+
+def is_same_class(obj, a_class):
+    """returns true if they are the same and false if not"""
+    if dir(obj) == dir(a_class):
+        return True
+    False
