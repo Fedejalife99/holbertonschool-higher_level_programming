4c96817d52aa899d19dc3c5a94dc64a910f1fc44
Federico Jalife <fedejalife@gmail.com>
2023-02-07 13:01:36 -0300

    task 4 fix comparation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,6 +3,6 @@
 
 def inherits_from(obj, a_class):
     """return true if inherit from a parent or false if not"""
-    if inherits_from(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
