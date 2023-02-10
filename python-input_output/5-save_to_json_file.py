a65b52989ef2a32825c8649c7433ef3841dfe7f7
Fedejalife99 <fedejalife@gmail.com>
2023-02-10 15:54:02 -0800

    task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a function that writes an Object to a text file using Json"""
+
+
+def save_to_json_file(my_obj, filename):
+    """function"""
+    with open(filename, 'w') as file:
+        """function"""
+        json.dump(my_obj, file)
