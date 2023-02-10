c51427084e34a6fdf07663e583fff54095aaebe5
Fedejalife99 <fedejalife@gmail.com>
2023-02-10 15:57:57 -0800

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,6 +2,7 @@
 """define a function that writes an Object to a text file using Json"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """function"""
     with open(filename, 'w') as file:
