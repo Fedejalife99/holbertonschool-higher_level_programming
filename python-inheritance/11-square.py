9348849230f29164dd24b272920e4eb79fb24d7f
Fedejalife99 <fedejalife@gmail.com>
2023-02-10 09:50:18 -0800

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class square that inherits from rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """create a square class"""
+
+    def __init__(self, size):
+        """initialize a square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+        self.__width = size
+        self.__height = size
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__():
+        return (f"[Square] {self.__size}/{self.__size}")
