e1017c42cc1d31dfe8291f59c1bfe536bdd3d16d
Fedejalife99 <fedejalife@gmail.com>
2023-02-11 06:18:42 -0800

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""""""
+"""define a class that represent a student"""
 
 
 class Student:
-    """"""
+    """Class student"""
     def __init__(self, first_name, last_name, age):
-        """"""
+        """initialize a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
