be3f2413a8a25f976a3d4e54dbd0a75c12b0808a
Fedejalife99 <fedejalife@gmail.com>
2023-02-11 05:07:48 -0800

    task 3 and 4

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that convert an string object to a json object"""
+import json
+
+
+def to_json_string(my_obj):
+    """fucntion"""
+    return json.dumps(my_obj)
---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""define a function that convert a Json string into a python object"""
+import json
+
+def from_json_string(my_str):
+    """function"""
+    return json.loads(my_str)
