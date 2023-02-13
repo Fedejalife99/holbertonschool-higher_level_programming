f43febee77b95dd8a7e320c48c245358a5382512
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-02-13 12:43:17 -0300

    task 1 pycodestyle fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """define a class to be the base from all the excersises"""
 
+
 class Base:
     """class base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
-            
+ 
         else:
             Base.__nb_objects += 1
-            self.id =  Base.__nb_objects
+            self.id = Base.__nb_objects
