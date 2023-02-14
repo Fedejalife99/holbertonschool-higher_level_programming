3476e2135e2a8eb5067935ee2bd1d803f68ede4b
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-02-14 13:39:14 -0300

    Task 2 square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """new Square"""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """get and set the value of the dimentions of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
     
     def __str__(self):
         """return a representation of the square"""
