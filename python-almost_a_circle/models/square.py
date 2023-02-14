b3badb72834761fd5d592f091bb981f28aac2733
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-02-14 13:27:36 -0300

    First task Square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""creates a class Square that inherits from rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """new Square"""
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """return a representation of the square"""
+        return(f"[Square] ({id} {self.x}/{self.y} {self.height}")
