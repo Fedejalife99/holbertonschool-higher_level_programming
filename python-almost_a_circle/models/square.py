22af3640b63e304d9875908db672820240fb9b42
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-02-15 12:04:39 -0300

    task 13 square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -21,4 +21,41 @@ class Square(Rectangle):
 
     def __str__(self):
         """return a representation of the square"""
-        return(f"[Square] ({id} {self.x}/{self.y} {self.height}")
+        return(f"[Square] ({self.id}) {self.x}/{self.y} {self.height}")
+
+    def update(self, *args, **kwargs):
+        """update the square"""
+        i = 1
+        if args and len(args) != 0:
+            for argument in args:
+                if i == 1:
+                    self.id = argument
+                if i == 2:
+                    self.__width = argument
+                if i == 3:
+                    self.__height = argument
+                if i == 4:
+                    self.__x = argument
+                if i == 5:
+                    self.__y = argument
+                i += 1          
+        elif kwargs and len(kwargs) != 0:
+            for key in kwargs:
+                for value in key:
+                    if key == 'id':
+                        self.id = value
+                    if key == 'size':
+                        self.size = value
+                    if key == 'x':
+                        self.__x = value
+                    if key == 'y':
+                        self.__y = value
+
+    def to_dictionary(self):
+        """return a dictionary representation of the square"""
+        new_dict = {'id': self.id, 
+                    'width': self.size, 
+                    'height': self.size, 
+                    'x': self.x, 
+                    'y': self.y}
+        return new_dict
