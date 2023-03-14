cbde9da22b5deab5d2b81e21633c6a1d5f142531
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-03-14 15:00:50 -0300

    task 11

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- show the scores that are bigger or equals to 10
-SELECT `name`, `score` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
