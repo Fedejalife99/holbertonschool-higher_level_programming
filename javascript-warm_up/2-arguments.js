3cfa7d591147f490ef9a1257ca2b989ed6a69fa5
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-04-12 23:51:33 -0300

    task 1, 2 & 3 fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 for (let i = 2; process.argv[i]; i++) {
-    if (process.argv[i]) {
+    if (process.argv[i] && process.argv[i + 1]) {
+        console.log('Arguments found');
+        return;
+    }
+    if (process.argv[i] && !process.argv[i + 1]) {
         console.log('Argument found');
         return;
     }
     else {
-        console.log('No argument')
+        console.log('No argument');
     }
 }
 console.log('No argument')
--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,8 +2,13 @@
 for (let i = 2; process.argv[i]; i++) {
     if (process.argv[i]) {
         console.log(process.argv[i]);
+        if (!process.argv[i + 1]) {
+            return
+        }
     }
     else {
-        console.log('No argument')
+        console.log('No argument');
+        return;
     }
 }
+console.log('No arguments');
